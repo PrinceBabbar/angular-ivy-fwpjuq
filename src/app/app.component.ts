b097e287a797ac539c41094475f492e37eaa8c7a
PrinceBabbar <prince.2930@gmail.com>
2021-08-21 16:25:56 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  isCheckedMale: boolean;
+  isCheckedFemale: boolean;
+  students: any = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+      this.students = data;
+    });
+
+    console.log(this.isCheckedMale + '' + this.isCheckedFemale);
+  }
+
+  checkOnChange($event) {
+    const val = $event.target.value;
+    console.log('values is' + val);
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get('assets/Input.json');
+  }
 }
